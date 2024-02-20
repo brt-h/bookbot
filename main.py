793212974d1f9e971b79ec1864f86c7cbe64fb34
Robert Hyman <13157542+brt-h@users.noreply.github.com>
2024-02-20 02:52:58 -0500

    add book report

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-print("hello world")    
-
 def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    book_count = count_words(book_text)
-    print(book_text)
-    print(book_count)
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    word_count = count_words(text)
+    letters_count_dict = count_letters(text)
+    letters_count_list = letter_count_sorted(letters_count_dict)
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document \n")
+    for item in letters_count_list:
+        print(f"The {item["letter"]} character was found {item["count"]} times")
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def count_words(text):
-    word_count = len(text.split())
-    print(f"The file contains {word_count} words.")
+    return len(text.split())
 
+def count_letters(text):
+    letter_counts = {}
+    for letter in text:
+        if letter.lower() in letter_counts:
+            letter_counts[letter.lower()] += 1
+        else:
+            letter_counts[letter.lower()] = 1
+    return(letter_counts)
+
+def letter_count_sorted(dictionary):
+    letter_counts_list = []
+    for item in dictionary:
+        if item.isalpha():
+            letter_counts_list.append({"letter": item, "count":dictionary[item]})
+    letter_counts_list.sort(reverse=True, key=lambda item: item["count"])
+    return letter_counts_list
+        
 if __name__ == "__main__":
     main()
